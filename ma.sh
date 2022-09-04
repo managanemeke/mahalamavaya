85b9f5b9a1d8cab6b3dddd1c8a243247e8816445
managanemeke <managanemeke@gmail.com>
2022-09-04 12:33:52 +0500

    takaga

--- ma.sh
+++ ma.sh
@@ -2,12 +2,13 @@
 
 docker build --rm=true --force-rm=true -t vaya ./vaya
 #
-# docker run -p 81:80 -p 444:443 \
-  # -v ./web:/home/aurora/web:ro \
-  # -v ./app:/home/aurora/app:ro \
-  # netureso:latest
-#
 docker-compose up --build -d
+#
+docker-compose exec vaya /bin/sh -c '\
+  npm install'
+#
+docker-compose exec vaya /bin/sh -c '\
+  npm run serve'
 #
 echo ""
 echo "check this:"
--- vaya/src/http-common.js
+++ vaya/src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "http://localhost:9000",
+  baseURL: "http://188.127.227.79:9000",
   headers: {
     "Content-type": "application/json"
   }
